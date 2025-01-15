0f75b7feb9c77ac3de16d39dd22eff0aa96ce4c5
asep <asepsunarya839@gmail.com>
2025-01-15 19:38:05 +0700

    fix: transaction to payment

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,10 @@
-import TransactionView from '@/views/user/TransactionView.vue'
+import PaymentView from '@/views/user/PaymentView.vue'
 import KtrRequestView from '../views/user/KtrRequestView.vue'
 import KtrRequestCreateView from '../views/user/KtrRequestCreateView.vue'
 import PaymentConfirmView from '../views/user/PaymentConfirmView.vue'
 
 export default [
-  { path: '/user/transaction', component: TransactionView },
+  { path: '/user/payment', component: PaymentView },
   { path: '/user/request', component: KtrRequestView },
   { path: '/user/request/create', component: KtrRequestCreateView },
   { path: '/user/payments/:id', component: PaymentConfirmView },
